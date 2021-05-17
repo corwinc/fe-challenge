97093c8e53c1bba73d672f991dcdeba542382c8b
corwinc <corwin.crownover@gmail.com>
2021-05-17 10:05:55 -0400

    Tweak styles

--- frontend/src/domain/Transactions/TransactionListItem/index.js
+++ frontend/src/domain/Transactions/TransactionListItem/index.js
@@ -6,26 +6,24 @@ import './style.css';
 const TransactionListItem = ({ transaction, handleLabelsClick }) => (
     <div className="Container">
         <div className="Container-left">
-            <div className="Data Text">{transaction.text}</div>
-            <div className="Data Parties-container">
+            <div className="Text">{transaction.text}</div>
+            <div className="Parties-container">
                 <div>{transaction.source}</div>
                 <div>&#10140;</div>
                 <div>{transaction.target}</div>
             </div>
+            <div className="Amount">{`$${transaction.amount}`}</div>
         </div>
-        <div className="Container-right">
-            <div className="Data Amount">{`$${transaction.amount}`}</div>
-            { transaction.labels.length == 0
-                ? <div className="Labels-add" onClick={() => handleLabelsClick(transaction)}>+</div>
-                : (
-                    <div className="Labels" onClick={() => handleLabelsClick(transaction)}>
-                        { transaction.labels.sort((a,b) => a.name > b.name).map(label => (
-                            <Label key={label.id} transactionId={transaction.id} label={label} />
-                        ))}
-                    </div>
-                )
-            }
-        </div>
+        { transaction.labels.length == 0
+            ? <div className="Labels-add" onClick={() => handleLabelsClick(transaction)}>+</div>
+            : (
+                <div className="Labels" onClick={() => handleLabelsClick(transaction)}>
+                    { transaction.labels.sort((a,b) => a.name > b.name).map(label => (
+                        <Label key={label.id} transactionId={transaction.id} label={label} />
+                    ))}
+                </div>
+            )
+        }
     </div>
 );
 
